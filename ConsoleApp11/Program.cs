4445293bdedd9c98ebf2cde0bcb8be0d4d2af404
Oybek Nematov <1377oybeknematov1377@gmail.com>
2025-06-20 03:22:01 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public abstract class Animal
+{
+    protected string Name;
+
+    public void SetName(string name)
+    {
+        Name = name;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+
+    public abstract void Eat();
+}
+
+public class Dog : Animal
+{
+    public override void Eat()
+    {
+        Console.WriteLine("Eating");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the dog's name: ");
+        string dogName = Console.ReadLine();
+
+        Dog myDog = new Dog();
+        myDog.SetName(dogName);
+
+        Console.WriteLine(myDog.GetName());
+        myDog.Eat();
+    }
+}
